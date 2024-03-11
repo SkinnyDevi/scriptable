6c6a7f869033019a02d80544acfbe14ad1939b47
SkinnyDevi <skinnydevi.dev@gmail.com>
2024-03-11 23:43:16 +0000

    Added new widget

---
+++ Amazon Product Viewer/README.md
@@ -0,0 +1,29 @@
+# Amazon Product Viewer - Widget
+## English
+Given a product URL such as:
+```
+https://www.amazon.es/Apple-iPhone-128GB-Negro-Reacondicionado/dp/B08PCCZB1H
+```
+This widget fetches the price and discount (if any) of the product of the URL and displays it in a widget.
+
+## Installation
+1. Download this script from the [releases section](https://github.com/SkinnyDevi/scriptable/releases).
+2. Open the script for editing.
+3. (Optional) Replace the product image in the `PRODUCT_IMAGE` variable with your own custom image URL.
+4. Make sure `USE_ENG` is set to `true`.
+5. Once you set the widget on your home screen, edit the widget and paste your product URL in the parameters section.
+
+
+## Español
+Dada una URL de cualquier producto por el estilo de:
+```
+https://www.amazon.es/Apple-iPhone-128GB-Negro-Reacondicionado/dp/B08PCCZB1H
+```
+Este widget busca en la página el precio y descuento (si hay) del product y lo muestra en el widget.
+
+## Instalacion
+1. Descarga este script desde la sección de [releases](https://github.com/SkinnyDevi/scriptable/releases) de GitHub.
+2. Abre este script para editarlo.
+3. (Opcional) Reemplaza la imagen del producto en la variable `PRODUCT_IMAGE` con una URL de tu imagen preferida.
+4. Asegurate que la variable `USE_ENG` esta puesta a `false`.
+5. Una vez hayas puesto el widget y seleccionado el script, pon la URL de tu producto en la zona de texto de "parametros".
---
+++ Amazon Product Viewer/script.ts
@@ -0,0 +1,263 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: orange; icon-glyph: chart-bar;
+// version: 1.0.0
+// author: SkinnyDevi (GitHub)
+
+import {
+  config,
+  Script,
+  ListWidget,
+  Font,
+  WebView,
+  Color,
+  Alert,
+  WidgetStack,
+  LinearGradient,
+  Request,
+  args,
+} from "nios-scriptable";
+
+// -------- USER CONFIG ------- //
+const PRODUCT_URL = args.widgetParameter;
+
+const WIDGET_BG = `
+https://media.istockphoto.com/id/1169630303/photo/blue-textured-background.webp?b=1&s=170667a&w=0&k=20&c=tI2xFhXqXFqMM0IvxSYY3F7LIwv450h2ch3yD-lZ9HU=
+`;
+
+const USE_ENG = true;
+const SHOW_TEXT = true;
+const USE_CUSTOM_IMAGE = false;
+
+// -------- MAIN CODE ---------- //
+async function mainScript() {
+  const displayer = await productFetcher();
+
+  if (config.runsInWidget) {
+    let widget = displayer.has_error
+      ? await createErrorWidget(displayer as ProductInfoError)
+      : await createWidget(displayer as ProductInfo, null);
+    Script.setWidget(widget);
+  } else {
+    const options = ["Small", "Medium", "Large", "Cancel"];
+    let resp = await presentAlert("Preview Widget", options, false);
+    if (resp == options.length - 1) return;
+    let size = options[resp];
+    let widget = displayer.has_error
+      ? await createErrorWidget(displayer as ProductInfoError)
+      : await createWidget(displayer as ProductInfo, size.toLowerCase());
+
+    switch (size) {
+      case "Small":
+        await widget.presentSmall();
+        break;
+      case "Medium":
+        await widget.presentMedium();
+        break;
+      case "Large":
+        await widget.presentLarge();
+        break;
+    }
+  }
+
+  Script.complete();
+}
+
+await mainScript();
+
+interface ProductInfo {
+  has_error: boolean;
+  imgUrl: string;
+  url: string;
+  text: string;
+}
+
+interface ProductInfoError {
+  has_error: boolean;
+  err_msg: string;
+}
+
+// -------- PRODUCT FETCHER --------- //
+async function productFetcher(): Promise<ProductInfo | ProductInfoError> {
+  if (PRODUCT_URL === null) {
+    return {
+      has_error: true,
+      err_msg: USE_ENG
+        ? "This text was displayed because no URL was specified."
+        : "No se ha proporcionado un enlace para buscar el producto.",
+    };
+  }
+
+  const htmlString = await new Request(PRODUCT_URL.trim()).loadString();
+
+  const dataFetcher = `
+		function fetchAmazonData() {
+			const productTitle = document.getElementById("productTitle").innerHTML;
+
+			let price = document.querySelector("#corePrice_feature_div > div > div > span.a-price.aok-align-center > span.a-offscreen");
+
+			if (price === null) {
+				const symbol = document.querySelector("span.a-price-symbol").innerHTML;
+				const whole = document.querySelector("span.a-price-whole").innerText;
+				const fraction = document.querySelector("span.a-price-fraction").innerHTML;
+				price = whole + fraction + symbol;
+			} else {
+				price = price.innerText;
+			}
+
+			const imgUrl = document.querySelector("#imgTagWrapperId > img").src;
+
+			const perc = document.querySelector("span.savingsPercentage");
+			let percentage = '0';
+			if (perc !== null) {
+				const percRaw = document.querySelector("span.savingsPercentage").innerText.split('');
+      	percentage = percRaw.slice(1, 3).join('');
+			}
+
+			const data = {
+				imgUrl: imgUrl,
+				price: price,
+				text: productTitle,
+				discount: percentage
+			};
+
+			return JSON.stringify(data);
+		}
+
+		fetchAmazonData();
+	`;
+
+  const webview = new WebView();
+  await webview.loadHTML(htmlString, PRODUCT_URL.trim());
+
+  let response = await webview.evaluateJavaScript(dataFetcher);
+
+  if (response === null) {
+    return {
+      has_error: true,
+      err_msg: "This text is displayed because there was an error",
+    };
+  }
+
+  response = JSON.parse(response);
+  console.log(response);
+
+  const prodName = response.text.substr(0, 21).trim();
+  const productPrice = response.price;
+  const discountP = response.discount;
+
+  const imageUrl =
+    (USE_CUSTOM_IMAGE ? WIDGET_BG : response.imgUrl) || WIDGET_BG;
+  const now = new Date();
+  const nowRefresh = now.toLocaleTimeString();
+
+  const priceLabel = USE_ENG ? "Price" : "Precio";
+  const discountLabel = USE_ENG ? "Discount" : "Descuento";
+  const checkLabel = USE_ENG ? "Last Check" : "Ultimo Checkeo";
+
+  return {
+    has_error: false,
+    imgUrl: imageUrl,
+    url: PRODUCT_URL.trim(),
+    text: `${prodName} - ${priceLabel}: ${productPrice}
+${discountLabel} ${discountP}%
+${checkLabel}: ${nowRefresh}`,
+  };
+}
+
+// -------- UTILS -------- //
+function addText(
+  container: WidgetStack,
+  text: string,
+  align: string,
+  size: number
+) {
+  const txt = container.addText(text);
+
+  switch (align) {
+    default:
+      txt.leftAlignText();
+      break;
+    case "center":
+      txt.centerAlignText();
+      break;
+    case "right":
+      txt.rightAlignText();
+      break;
+  }
+
+  txt.font = Font.systemFont(size);
+  txt.shadowRadius = 3;
+  txt.textColor = Color.white();
+  txt.shadowColor = Color.black();
+}
+
+function newLinearGradient(hexcolors: string[], locations: number[]) {
+  let gradient = new LinearGradient();
+  gradient.locations = locations;
+  gradient.colors = hexcolors.map((color) => new Color(color));
+  return gradient;
+}
+
+async function presentAlert(prompt: string, items: string[], asSheet: boolean) {
+  let alert = new Alert();
+  alert.message = prompt;
+
+  for (const item of items) {
+    alert.addAction(item);
+  }
+  return asSheet ? await alert.presentSheet() : await alert.presentAlert();
+}
+
+// -------- WIDGET -------- //
+async function createWidget(data: ProductInfo, widgetFamily: string | null) {
+  widgetFamily = widgetFamily || config.widgetFamily;
+  const padd = widgetFamily == "large" ? 12 : 10;
+  const fontSize = 18;
+
+  const widget = new ListWidget();
+  const req = new Request(data.imgUrl.trim());
+  const img = await req.loadImage();
+
+  const refreshRate = 5; // min
+
+  const refreshDate = Date.now() + 1000 * 60 * refreshRate;
+  widget.refreshAfterDate = new Date(refreshDate);
+
+  widget.url = data.url;
+  widget.setPadding(padd, padd, padd, padd);
+  widget.backgroundImage = img;
+
+  if (SHOW_TEXT) {
+    // add gradient with a semi-transparent
+    // dark section at the bottom. this helps
+    // with the readability of the status line
+    widget.backgroundGradient = newLinearGradient(
+      ["#ffffff00", "#ffffff00", "#00000088"],
+      [0, 0.8, 1]
+    );
+
+    // top spacer to push the bottom stack down
+    widget.addSpacer();
+
+    // horizontal stack to hold the status line
+    const stats = widget.addStack();
+    stats.layoutHorizontally();
+    stats.centerAlignContent();
+    stats.spacing = 3;
+
+    addText(stats, data.text, "left", fontSize);
+    stats.addSpacer();
+  }
+  return widget;
+}
+// -------- ERROR WIDGET -------- //
+async function createErrorWidget(data: ProductInfoError) {
+  const widget = new ListWidget();
+  widget.addSpacer();
+  const text = widget.addText(data.err_msg);
+  text.textColor = Color.white();
+  text.centerAlignText();
+  widget.addSpacer();
+  return widget;
+}
